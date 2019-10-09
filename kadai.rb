d1c5a8b82567f8989111f80e7f7a1c886d51bb2b
yariwake2000 <jalksmihby76gyt@gmail.com>
2019-10-09 23:57:54 +0900

    add kadai(ruby)

--- kadai.rb
+++ kadai.rb
@@ -1,12 +1,34 @@
-#! /usr/bin/env ruby
+#s /usr/bin/env ruby
 
 rika = [65, 80, 67, 35, 58, 60, 72, 75, 68, 92, 36, 50, 2, 58.5, 46, 42, 78, 62, 84, 70]
 eigo = [44, 87, 100, 63, 52, 60, 58, 73, 55, 86, 29, 56, 89, 23, 65, 84, 64, 27, 86, 84]
-rsum = 0
-esum = 0
 
-(0..19).each{|i|
-	rsum += rika[i]
-}
-p rsum
+class Array
+  def mean
+    sum.to_f / size
+  end
+
+  def var
+    m = mean
+    reduce(0) { |a, b| a + (b - m) ** 2 } / (size - 1)
+  end
+
+  def stdev
+    Math.sqrt(var)
+  end
+
+  def stsco
+    m = mean
+    s = stdev
+    map { |n| 10 * (n - m) / s + 50 } 
+  end
+
+  def sort_desc
+    sort { |a, b| b <=> a }
+  end
+end
 	
+p rika.sum, rika.mean, rika.stdev
+p eigo.sum, eigo.mean, eigo.stdev
+p rika.stsco
+p rika.sort_desc
