e7cc9c65d1d294b3abd55db0715d73c50ff65e6c
yariwake2000 <jalksmihby76gyt@gmail.com>
2019-10-10 16:35:08 +0900

    fix kadai(ruby)

--- kadai.rb
+++ kadai.rb
@@ -4,26 +4,26 @@ rika = [65, 80, 67, 35, 58, 60, 72, 75, 68, 92, 36, 50, 2, 58.5, 46, 42, 78, 62,
 eigo = [44, 87, 100, 63, 52, 60, 58, 73, 55, 86, 29, 56, 89, 23, 65, 84, 64, 27, 86, 84]
 
 class Array
-  def mean
+  def mean#平均値
     sum.to_f / size
   end
 
-  def var
+  def var#分散
     m = mean
     reduce(0) { |a, b| a + (b - m) ** 2 } / size
   end
 
-  def stdev
+  def stdev#標準偏差
     Math.sqrt(var)
   end
 
-  def stsco
+  def stsco#偏差値
     m = mean
     s = stdev
     map { |n| 10 * (n - m) / s + 50 } 
   end
 
-  def sort_desc
+  def sort_desc#降順ソート
     sort { |a, b| b <=> a }
   end
 end
